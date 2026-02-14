143285db5670e09fe34ca0426c1ecf71f6c77ab7
lucieross <l.ross20@rgu.ac.uk>
2026-02-14 16:34:45 +0000

    Basic App Setup - instructions in App.js

---
+++ Product/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//To run server use npm start in terminal
+//To see what it looks like in app form, right click inspect and click on the app tab
+// ctrl + c to stop server
+
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to ThriftApp</h1>
+    </div>
+  );
+}
+
+export default App;
