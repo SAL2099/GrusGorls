dbe16daabbf2b3014be8ab0875581322fdfcea1c
lucieross <l.ross20@rgu.ac.uk>
2026-02-16 13:13:17 +0000

    Updated instructions

--- Product/src/App.js
+++ Product/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//First navigate to GrusGorls folder in terminal using cd then navigate to Product folder using cd Product
 //To run server use npm start in terminal
 //To see what it looks like in app form, right click inspect and click on the app tab
 // ctrl + c to stop server
