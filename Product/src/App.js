1221361e647725d7275d58d773157b271a8ac1da
Lucieross <l.ross20@rgu.ac.uk>
2026-02-27 09:11:01 +0000

    Instructions again :)

--- Product/src/App.js
+++ Product/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './components/Header';
 import BottomNav from './components/BottomNav'
 
+//Npm install react-scripts
 //First navigate to GrusGorls folder in terminal using cd then navigate to Product folder using cd Product
 //To run server use npm start in terminal
 //To see what it looks like in app form, right click inspect and click on the app tab
